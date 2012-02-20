d9d671e8e6df13757bc84155a6f15241bb3edd64
Brian Maissy <brian.maissy@gmail.com>
2012-02-20 06:44:37 -0800

    added some jquery and ajax to make an extremely simple dot-moving UI

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var express = require("express");
 var ejs = require("ejs");
 
 var app = express.createServer();
+app.configure(function() {
+    app.use(express.bodyParser());
+    app.use(express.static('./static/'));
+    app.use(app.router);
+});
 app.set('view engine', 'ejs');
 app.set('view options', {
     layout: false
@@ -18,10 +23,10 @@ app.get('/', function(req, res){
     });
 });
 
-app.post('/', function(req, res){
-    xPos = req.body.pos['x'];
-    yPos = req.body.pos['y'];
-    res.render('index', {
+app.post('/input', function(req, res){
+    xPos += 10*parseInt(req.param('x', 0));
+    yPos += 10*parseInt(req.param('y', 0));
+    res.render('update', {
         x: xPos,
         y: yPos
     });
