b3709fd4f6c51b2fb17b62b7d5b9b829df22bb23
Brian Maissy <brian.maissy@gmail.com>
2012-02-23 23:59:35 -0800

    added socket.io support, which can be turned on or off (since it doesn't work on our phones yet, also added jquery animation and a new mouse interface

--- server.js
+++ server.js
@@ -13,23 +13,61 @@ app.set('view options', {
 });
 app.use(express.bodyParser());
 
+var sockets = false;
+
+if(sockets){
+    var io = require('socket.io').listen(app);
+}
+app.listen(8888);
+
 var xPos = 0;
 var yPos = 0;
 
 app.get('/', function(req, res){
     res.render('index', {
         x: xPos,
-        y: yPos
+        y: yPos,
+        sockets: sockets
     });
 });
 
-app.post('/input', function(req, res){
-    xPos += 10*parseInt(req.param('x', 0));
-    yPos += 10*parseInt(req.param('y', 0));
-    res.render('update', {
-        x: xPos,
-        y: yPos
+if(sockets){
+    io.sockets.on('connection', function (socket) {
+        socket.on('location', function (data) {
+            socket.emit('location', { x: xPos, y: yPos });
+        });
+        socket.on('nudge', function (data) {
+            xPos += data.x;
+            yPos += data.y;
+            socket.emit('location', { x: xPos, y: yPos });
+        });
+        socket.on('move', function (data) {
+            xPos = data.x;
+            yPos = data.y;
+            socket.emit('location', { x: xPos, y: yPos });
+        });
     });
-});
-
-app.listen(8888);
+}else{
+    app.get('/location', function(req, res){
+        res.render('update', {
+            x: xPos,
+            y: yPos
+        });
+    });
+    app.post('/nudge', function(req, res){
+        xPos += 10*parseInt(req.param('x', 0));
+        yPos += 10*parseInt(req.param('y', 0));
+        res.render('update', {
+            x: xPos,
+            y: yPos
+        });
+    });
+    app.post('/move', function(req, res){
+        xPos = parseInt(req.param('x', 0));
+        yPos = parseInt(req.param('y', 0));
+        res.render('update', {
+            x: xPos,
+            y: yPos
+        });
+    });
+}
