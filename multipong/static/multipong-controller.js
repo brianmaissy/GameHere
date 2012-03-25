09711d301412c45fc311f57f4e55ee60a46550d2
Brian Maissy <brian.maissy@gmail.com>
2012-03-25 04:56:53 -0700

    added an index page to redirect to the controller. moved the controller and display logic out of the html files and into separate js files. fixed some display issues caused by the borders which made it look like objects were off by a couple pixels

---
+++ multipong/static/multipong-controller.js
@@ -0,0 +1,72 @@
+var socket;
+var name;
+$(document).ready(function(){
+    name = prompt("What is your name?", "anonymous");
+
+    // start the socket.io connection and set up the handlers
+    socket = io.connect('http://' + window.location.host);
+    socket.on('connect', function(){
+        $('div#message').html('Controller connected to server');
+        socket.emit('newPlayer', {name: name});
+    });
+    socket.on('playerConnected', function(data){
+        $('div#message').html(data.title + ' controller connected to display as player ' + name);
+    });
+});
+$(document).keypress(function(event) {
+    var deltaY = 0;
+    switch(parseInt(event.keyCode)){
+        case 38:
+        case 97:
+            deltaY = -1;
+            break;
+        case 40:
+        case 122:
+            deltaY = 1;
+            break;
+        case 0:
+            if(event.charCode == 112) socket.emit('pause');
+            break;
+        default:
+            return;
+    }
+    if(deltaY != 0){
+        socket.emit('move', {y: deltaY});
+    }
+});
+
+
+//TODO: add touch input
+var fingerDown = false;
+var dir = false;
+var startY = 0;
+var moveY = 0;
+var moveThreshold = 10;
+
+console.log("test");
+
+document.addEventListener('touchstart', function(e) {
+    startY = e.touches[0].pageY;
+});
+document.addEventListener('touchmove', function(e) {
+    moveY = e.touches[0].pageY;
+    if(Math.abs(moveY - startY) > moveThreshold) {
+        dir = (moveY > startY) ? 'down' : 'up';
+        if (dir == 'up')
+        {
+            deltaY = -1;
+        }
+        else
+        {
+            deltaY = 1;
+        }
+    }
+    else {
+        deltaY = 0;
+    }
+    socket.emit('move', {y: deltaY});
+});
+document.addEventListener('touchend', function(e) {
+    deltaY = 0;
+    socket.emit('move', {y: deltaY});
+});
---
+++ multipong/static/multipong-display.js
@@ -0,0 +1,116 @@
+var socket;
+var game;
+var players = {};
+var h = 0;
+var w = 0;
+var ball;
+document.addEventListener("DOMContentLoaded", function(){
+    // create the game and initialize things based on it
+    game = createGame();
+    var field = document.getElementById("field");
+    w = field.offsetWidth;
+    h = field.offsetHeight;
+    ball = document.getElementById("ball");
+    ball.style.width = h * game.ballRadius*2 - 2; // small correction for borders
+    ball.style.height = h * game.ballRadius*2 - 2;
+    ball.style.borderRadius = h * game.ballRadius;
+    ball.style.mozBorderRadius = h * game.ballRadius;
+
+    // start the socket.io connection and set up the handlers
+    socket = io.connect('http://' + window.location.host);
+    // the display's own connection handshake
+    socket.on('connect', function(){
+        document.getElementById("message").innerHTML = 'Display connected to server';
+        socket.emit('newDisplay');
+    });
+    // a new controller has connected through the server, create a player for it
+    socket.on('newPlayer', function(data){
+        players[data.controllerID] = game.newPlayer(data.name);
+        socket.emit('playerConnected', {controllerID: data.controllerID, title: game.title});
+    });
+    // a client has disconnected from the server
+    socket.on('clientDisconnect', function(data){
+        if(game.leftPlayers.indexOf(players[data.controllerID]) >= 0
+            || game.rightPlayers.indexOf(players[data.controllerID]) >= 0){
+            game.removePlayer(players[data.controllerID]);
+        }
+    });
+    // motion input from a controller through the server
+    socket.on('move', function(data){
+        if(!game.paused) players[data.controllerID].move(data);
+    });
+    socket.on('pause', function(){
+        game.pause();
+    });
+    // set up the Bond debugger
+    Bond.startRemoteClient(socket);
+
+    // start ticking away
+    setInterval(tick, 20);
+}, false);
+
+function tick(){
+    // tick the game
+    game.tick();
+    // update the display
+    var players = document.getElementsByClassName("player");
+    var i;
+    if(players.length > 0){
+        for(i=0; i<players.length; i++){
+            players[i].style.display = "none";
+        }
+    }
+    for(i=0; i<game.leftPlayers.length; i++){
+        drawPlayer(game.leftPlayers[i], "left", i);
+    }
+    for(i=0; i<game.rightPlayers.length; i++){
+        drawPlayer(game.rightPlayers[i], "right", i);
+    }
+    ball.style.top = h * (game.ballLocationY - game.ballRadius);
+    ball.style.left = w * game.ballLocationX - h * game.ballRadius;
+    document.getElementById("leftScore").innerHTML = game.leftScore;
+    document.getElementById("rightScore").innerHTML = game.rightScore;
+    var flash = document.getElementById("flash");
+    if(game.paused){
+        flash.innerHTML = "PAUSED";
+    }else if(game.starting){
+        flash.innerHTML = "GET READY";
+    }else{
+        flash.innerHTML = "";
+    }
+    var playerList = '';
+    for(i=0; i< game.leftPlayers.length; i++){
+        playerList += '<span style="color:' + game.leftPlayers[i].color + ';">' + game.leftPlayers[i].name + '</span><br>';
+    }
+    document.getElementById("leftPlayers").innerHTML = playerList;
+    playerList = '';
+    for(i=0; i< game.rightPlayers.length; i++){
+        playerList += '<span style="color:' + game.rightPlayers[i].color + ';">' + game.rightPlayers[i].name + '</span><br>';
+    }
+    document.getElementById("rightPlayers").innerHTML = playerList;
+
+}
+function drawPlayer(player, side, number){
+    // create a div if it doesn't yet exist, then adjust its position and color
+    var div = document.getElementById(side + number);
+    if(!div){
+        var container = document.getElementById("leftField");
+        if(side == "right"){
+            container = document.getElementById("rightField");
+        }
+        div = document.createElement('div');
+        div.setAttribute('class', 'player');
+        div.setAttribute('id', side + number);
+        div.style.height = h * game.paddleWidth - 4; // small correction for borders
+        div.style.width = w * game.paddleThickness - 4;
+        container.appendChild(div)
+    }
+    if(side == "right"){
+        div.style.left = w * (player.x - 1.0/2 - game.paddleThickness/2);
+    }else{
+        div.style.left = w * (player.x - game.paddleThickness/2);
+    }
+    div.style.top = h * player.y + 1;
+    div.style.borderColor = player.color;
+    div.style.display = "block";
+}
