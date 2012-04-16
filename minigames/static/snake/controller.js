9790b8da98d880adbfd805912594fef19a3a4b19
Mark Lu <marklu@marklu.net>
2012-04-16 01:20:07 -0700

    removed some redundant code

--- minigames/static/snake/controller.js
+++ minigames/static/snake/controller.js
@@ -197,52 +197,6 @@ function initTouch()
    document.addEventListener("touchcancel", touchHandler, true);
 }
 
-// (function($){
-// 	// Determine if we on iPhone or iPad
-// 	var isiOS = false;
-// 	var agent = navigator.userAgent.toLowerCase();
-// 	if(agent.indexOf('iphone') >= 0 || agent.indexOf('ipad') >= 0){
-// 		   isiOS = true;
-// 	}
-//  
-// 	$.fn.doubletap = function(onDoubleTapCallback, onTapCallback, delay){
-// 		var eventName, action;
-// 		delay = delay == null? 500 : delay;
-// 		eventName = isiOS == true? 'touchend' : 'click';
-//  
-// 		$(this).bind(eventName, function(event){
-// 			var now = new Date().getTime();
-// 			var lastTouch = $(this).data('lastTouch') || now + 1 /** the first time this will make delta a negative number */;
-// 			var delta = now - lastTouch;
-// 			clearTimeout(action);
-// 			if(delta<500 && delta>0){
-// 				if(onDoubleTapCallback != null && typeof onDoubleTapCallback == 'function'){
-// 					onDoubleTapCallback(event);
-// 				}
-// 			}else{
-// 				$(this).data('lastTouch', now);
-// 				action = setTimeout(function(evt){
-// 					if(onTapCallback != null && typeof onTapCallback == 'function'){
-// 						onTapCallback(evt);
-// 					}
-// 					clearTimeout(action);   // clear the timeout
-// 				}, delay, [event]);
-// 			}
-// 			$(this).data('lastTouch', now);
-// 		});
-// 	};
-// })(jQuery);
-
-document.addEventListener('touchend', function(e) {
-    deltaY = 0;
-	deltaX = 0;
-    socket.emit('move', {x: deltaX, y: deltaY});
-});
-
-function blockMove() {
-	event.preventDefault();
-}
-
 function drawInventory(inventory){
     var inv = document.getElementById('inventory');
     // inv.innerHTML = "";
